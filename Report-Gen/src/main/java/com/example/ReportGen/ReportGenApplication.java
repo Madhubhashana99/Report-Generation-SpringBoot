777897d1d13cdcc8d0103b87de2cade5b5cf2f0e
Madhubhashana Mahindarathne <104136158+Madhubhashana99@users.noreply.github.com>
2023-12-30 23:20:39 +0530

    just begin

---
+++ Report-Gen/src/main/java/com/example/ReportGen/ReportGenApplication.java
@@ -0,0 +1,13 @@
+package com.example.ReportGen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportGenApplication.class, args);
+	}
+
+}
